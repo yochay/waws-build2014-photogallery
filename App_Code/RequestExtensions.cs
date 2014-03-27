de749717a82a4e359161099b7ca1ba12979d868f
Ahmed ElSayed <ahmels@microsoft.com>
2014-03-27 14:26:09 -0700

    switching users to GUID Based

---
+++ App_Code/RequestExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+
+public static class RequestExtensions
+{
+    public static string GetCurrentUser(this HttpRequestBase request, HttpResponseBase response = null)
+    {
+        var userName = String.Empty;
+        if (request.Cookies["GuidUser"] != null)
+        {
+            userName = request.Cookies["GuidUser"].Value;
+            var db = Database.Open("PhotoGallery");
+            var guidUser = db.QuerySingle("SELECT * FROM GuidUsers WHERE UserName = @0", userName);
+            if (guidUser == null || guidUser.TotalLikes > 5)
+            {
+                userName = CreateNewUser();
+            }
+        }
+        else
+        {
+            userName = CreateNewUser();
+        }
+        if (response != null)
+            response.AddUser(userName);
+        return userName;
+    }
+
+    private static string CreateNewUser()
+    {
+        var newUser = Guid.NewGuid();
+        var db = Database.Open("PhotoGallery");
+        db.Execute(@"INSERT INTO GuidUsers (UserName, TotalLikes) VALUES (@0, @1)", newUser.ToString(), 0);
+        return newUser.ToString();
+    }
+}
---
+++ App_Code/ResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public static class ResponseExtensions
+{
+    public static void AddUser(this HttpResponseBase response, string userName)
+    {
+        var userCookie = new HttpCookie("GuidUser")
+        {
+            Value = userName,
+            Expires = DateTime.UtcNow.AddYears(1)
+        };
+        response.Cookies.Add(userCookie);
+    }
+}
